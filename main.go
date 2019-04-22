f26618a72e84848bb378babe86cd927e76176a79
dimitar <dimitar.manev@dxc.com>
2019-04-22 12:17:38 +0300

    Included Vessel Client

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 
 import (
 
+	"context"
 	"fmt"
+    "log"
+    "sync"
 
 	// Import the generated protobuf code
 	pb "github.com/dmanev/shippy-service-consignment/proto/consignment"
+    vesselProto "github.com/dmanev/shippy-service-vessel/proto/vessel"
     "github.com/micro/go-micro"
-	"context"
+)
+
+const (
+    port = ":50051"
 )
 
 type repository interface {
@@ -19,13 +26,16 @@ type repository interface {
 // Repository - Dummy repository, this simulates the use of a datastore
 // of some kind. We'll replace this with a real implementation later on.
 type Repository struct {
+    mu           sync.RWMutex
 	consignments []*pb.Consignment
 }
 
 // Create a new consignment
 func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+    repo.mu.Lock()
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
+    repo.mu.Unlock()
 	return consignment, nil
 }
 
@@ -39,12 +49,25 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 // to give you a better idea.
 type service struct {
 	repo repository
+    vesselClient vesselProto.VesselServiceClient
 }
 
 // CreateConsignment - we created just one method on our service,
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+
+	// Here we call a client instance of our vessel service with our consignment weight,
+	// and the amount of containers as the capacity value
+    vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+        MaxWeight: req.Weight,
+        Capacity: int32(len(req.Containers)),
+    })
+    log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+    if err != nil {
+        return err
+    }
 
 	// Save our consignment
 	consignment, err := s.repo.Create(req)
@@ -80,7 +103,9 @@ func main() {
     // Init will parse the command line flags.
     srv.Init()
 
-    pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+    vesselClient := vesselProto.NewVesselServiceClient("shippy.service.vessel", srv.Client())
+
+    pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
     // Run the server
     if err := srv.Run(); err != nil {
